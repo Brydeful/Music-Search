24221964893f528e2324bc68fd8a847a19c896ea
brydeful <slava8055@gmail.com>
2019-12-03 19:03:00 +0500

    Initial Commit

---
+++ Music Search/View Controller/MusicTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MusicTableViewController.swift
+//  Music Search
+//
+//  Created by Вячеслав on 03.12.2019.
+//  Copyright © 2019 Вячеслав. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class MusicTableViewController: UITableViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+}
